d04de138cefab14aa06fb6f81d2098cd161be955
Dochi08 <123103046+Dochi08@users.noreply.github.com>
2023-04-13 09:29:51 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ namespace SpriteKind {
     export const question = SpriteKind.create()
     export const question2 = SpriteKind.create()
     export const empoweredPlayer = SpriteKind.create()
+    export const dougenentrence = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (sprite, location) {
     questionbox = sprites.create(img`
@@ -45,6 +46,17 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (spr
     tiles.setCurrentTilemap(tilemap2[0])
     tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.stairSouth)
 })
+sprites.onOverlap(SpriteKind.empoweredPlayer, SpriteKind.dougenentrence, function (sprite, otherSprite) {
+    game.showLongText("Greetings! Do you want to go into the hell of dogends", DialogLayout.Bottom)
+    story.showPlayerChoices("Yes I am ready", "Give me some time")
+    if (story.checkLastAnswer("Yes I am ready")) {
+        tiles.setCurrentTilemap(tilemap2[3])
+        sprites.destroy(doungenentrence)
+        fightScene(sprite)
+    } else {
+        game.showLongText("Okay have some time then", DialogLayout.Center)
+    }
+})
 scene.onOverlapTile(SpriteKind.empoweredPlayer, sprites.dungeon.stairNorth, function (sprite, location) {
     game.showLongText("You may no longer enter", DialogLayout.Center)
     pause(500)
@@ -56,6 +68,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.question, function (sprite, othe
     game.showLongText("this is a place that you get your super power", DialogLayout.Bottom)
     sprites.destroy(otherSprite)
 })
+function fightScene (mySprite: Sprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(19, 5))
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.question2, function (sprite, otherSprite) {
     game.showLongText("CHOOSE YOUR POWER", DialogLayout.Bottom)
     story.showPlayerChoices("Fire", "Ice", "Earth")
@@ -88,6 +103,13 @@ function earthwizard2 () {
         `, SpriteKind.empoweredPlayer)
     spriteCreator(earthwizard)
 }
+sprites.onOverlap(SpriteKind.empoweredPlayer, SpriteKind.Enemy, function (sprite, otherSprite) {
+    tiles.placeOnTile(sprite, tiles.getTileLocation(10, 5))
+    tiles.placeOnTile(otherSprite, tiles.getTileLocation(8, 5))
+    otherSprite.follow(sprite, 0)
+    color.startFade(color.originalPalette, color.originalPalette)
+    color.setColor(1, color.rgb(255, 255, 255))
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     earthwizard2()
     afterpower(earthwizard)
@@ -122,7 +144,7 @@ function afterpower (mySprite: Sprite) {
     sprites.destroy(_1st_sprrite)
     sprites.destroy(questionbo_x)
     sprites.destroy(questionbox)
-    tiles.setCurrentTilemap(tilemap2[1])
+    tiles.setCurrentTilemap(tilemap2[2])
     tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 9))
     doungenentrence = sprites.create(img`
         . . . . . . f f f f . . . . . . 
@@ -141,9 +163,44 @@ function afterpower (mySprite: Sprite) {
         . . 5 5 f 5 5 5 5 5 5 f 5 5 . . 
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
+        `, SpriteKind.dougenentrence)
     tiles.placeOnTile(doungenentrence, tiles.getTileLocation(5, 1))
 }
+scene.onOverlapTile(SpriteKind.empoweredPlayer, sprites.dungeon.darkGroundEast, function (sprite, location) {
+    if (numbercounter == 1) {
+        Firstenemy = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........fffff.........
+            ........ff11111f........
+            .......fb111111bf.......
+            ......fbd1111111f.......
+            ......fddd111111df......
+            ......fdddd11111df......
+            ......fddddddd11df......
+            ......fddddddd111f......
+            ......fddddddcf11f......
+            .......fbdddb1111bf.....
+            ........fffcfdb1b1f.....
+            .......ffffffffbfbf.....
+            ....ff.fffffffffff......
+            .....ffffffff...........
+            .....ffffffb1b1f........
+            ......ffffffbfbf........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        Firstenemy.follow(sprite, 1000)
+        scene.cameraShake(4, 3000)
+        game.showLongText("Fear from my power", DialogLayout.Bottom)
+        numbercounter = numbercounter + 1
+    }
+    tiles.placeOnTile(Firstenemy, tiles.getTileLocation(9, 5))
+})
 function icewizard2 () {
     icewizard = sprites.create(img`
         . . . . . . f f f f . . . . . . 
@@ -174,11 +231,13 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
     afterpower(icewizard)
 })
 let icewizard: Sprite = null
-let doungenentrence: Sprite = null
+let Firstenemy: Sprite = null
 let firewizard: Sprite = null
 let earthwizard: Sprite = null
+let doungenentrence: Sprite = null
 let questionbo_x: Sprite = null
 let questionbox: Sprite = null
+let numbercounter = 0
 let tilemap2: tiles.TileMapData[] = []
 let _1st_sprrite: Sprite = null
 _1st_sprrite = sprites.create(img`
@@ -199,8 +258,14 @@ _1st_sprrite = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-tiles.setCurrentTilemap(tilemap`level13`)
+tiles.setCurrentTilemap(tilemap`level5`)
 tiles.placeOnRandomTile(_1st_sprrite, sprites.builtin.forestTiles22)
 scene.cameraFollowSprite(_1st_sprrite)
 controller.moveSprite(_1st_sprrite)
-tilemap2 = [tilemap`level1`, tilemap`level13`]
+tilemap2 = [
+tilemap`level1`,
+tilemap`level5`,
+tilemap`level13`,
+tilemap`level27`
+]
+numbercounter = 1
