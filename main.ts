ebf0e06ebbbe10959460024ac058c4819f0360ac
Dochi08 <123103046+Dochi08@users.noreply.github.com>
2023-04-27 07:50:10 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,44 @@ namespace SpriteKind {
     export const empoweredPlayer = SpriteKind.create()
     export const dougenentrence = SpriteKind.create()
 }
+function QuizSubstraction (num1: number, num2: number) {
+    substraction1 = game.askForNumber("Hello" + "World" + "")
+    if (substraction1 == num1 - num2) {
+        game.splash("Correct Answer!!")
+        if (correctcounter >= 2) {
+            pause(200)
+            for (let index = 0; index < 2; index++) {
+                damagecoin2 = sprites.createProjectileFromSprite(img`
+                    . . . . . . . 5 . . . . . . . . 
+                    . . . . . . 5 4 5 . . . . . . . 
+                    . . . . . 5 4 4 4 5 . . . . . . 
+                    . . . . 5 4 4 4 4 4 5 . . . . . 
+                    . . . 5 4 4 4 4 4 4 4 5 . . . . 
+                    . . 5 4 5 4 5 4 5 5 5 4 5 . . . 
+                    . 5 4 4 5 4 5 4 5 4 5 4 4 5 . . 
+                    5 4 4 4 5 5 5 4 5 4 5 4 4 4 5 . 
+                    . 5 4 4 4 4 5 4 5 4 5 4 4 5 . . 
+                    . . 5 4 4 4 5 4 5 5 5 4 5 . . . 
+                    . . . 5 4 4 4 4 4 4 4 5 . . . . 
+                    . . . . 5 4 4 4 4 4 5 . . . . . 
+                    . . . . . 5 4 4 4 5 . . . . . . 
+                    . . . . . . 5 4 5 . . . . . . . 
+                    . . . . . . . 5 . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, Firstenemy, 0, -50)
+                pause(500)
+            }
+            for (let index = 0; index < 2; index++) {
+                statusbarMysprite.value += 20
+            }
+            pause(250)
+            correctcounter += 1
+            sprites.destroy(damagecoin2)
+        }
+    } else {
+        game.splash("Wrong Answer!")
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (sprite, location) {
     questionbox = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -57,6 +95,7 @@ sprites.onOverlap(SpriteKind.empoweredPlayer, SpriteKind.dougenentrence, functio
         game.showLongText("Okay have some time then", DialogLayout.Center)
     }
 })
+// Code written by Nate Au.
 scene.onOverlapTile(SpriteKind.empoweredPlayer, sprites.dungeon.stairNorth, function (sprite, location) {
     game.showLongText("You may no longer enter", DialogLayout.Center)
     pause(500)
@@ -68,7 +107,115 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.question, function (sprite, othe
     game.showLongText("this is a place that you get your super power", DialogLayout.Bottom)
     sprites.destroy(otherSprite)
 })
-function earthCommonSpell2 () {
+// Code written by Nate Au.
+function spriteController (mySprite: Sprite) {
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+    mySprite.setFlag(SpriteFlag.StayInScreen, true)
+}
+// Code written by Nate Au.
+function fightScene (mySprite: Sprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(19, 5))
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.question2, function (sprite, otherSprite) {
+    game.showLongText("CHOOSE YOUR POWER", DialogLayout.Bottom)
+    story.showPlayerChoices("Fire", "Ice", "Earth")
+    if (story.checkLastAnswer("Fire")) {
+        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleRedCrystal)
+    } else if (story.checkLastAnswer("Ice")) {
+        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleInsignia)
+    } else if (story.checkLastAnswer("Earth")) {
+        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleBlueCrystal)
+    }
+})
+function earthwizard2 () {
+    earthwizard = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 7 7 f f f . . . . 
+        . . . f f f 7 8 8 7 f f f . . . 
+        . . f f f 7 8 7 7 8 7 f f f . . 
+        . . f f 7 7 8 7 7 8 7 7 7 f . . 
+        . . f 7 7 f f f f f f 7 7 f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . f f e e d d d d d d e e f f 7 
+        7 . . f e e 4 4 4 4 e e f . . 7 
+        7 7 7 7 f 7 8 8 8 8 7 f 7 7 7 7 
+        7 7 7 7 f 7 8 8 8 8 7 f 7 7 7 7 
+        . . . . f 7 7 5 5 7 7 f 7 7 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.empoweredPlayer)
+    spriteController(earthwizard)
+    spritetype = "earth"
+}
+// Code written by Nate Au.
+sprites.onOverlap(SpriteKind.empoweredPlayer, SpriteKind.Enemy, function (sprite, otherSprite) {
+    color.startFade(color.originalPalette, color.Black, 500)
+    tiles.placeOnTile(sprite, tiles.getTileLocation(10, 5))
+    tiles.placeOnTile(otherSprite, tiles.getTileLocation(8, 5))
+    otherSprite.follow(sprite, 0)
+    fightscene12(sprite)
+    statusbarMysprite = statusbars.create(20, 4, StatusBarKind.Health)
+    statusbarMysprite.value = 100
+    statusbarMysprite.attachToSprite(firewizard)
+    statusbarFirstEnemy = statusbars.create(20, 4, StatusBarKind.Health)
+    statusbarFirstEnemy.value = 40
+    statusbarFirstEnemy.attachToSprite(Firstenemy)
+    color.startFade(color.Black, color.originalPalette, 500)
+    pause(450)
+    if (correctcounter <= 2) {
+        QuizAddition(randint(1, 10), randint(1, 10))
+    } else {
+        QuizSubstraction(randint(10, 20), randint(1, 10))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    spriteCreator("earth")
+    afterpower(earthwizard)
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.gameOver(false)
+})
+function spriteCreator (text: string) {
+    if (text == "fire") {
+        firewizard2()
+    } else if (text == "ice") {
+        icewizard2()
+    } else {
+        earthwizard2()
+    }
+}
+function fightscene12 (mySprite: Sprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(13, 5))
+    controller.moveSprite(mySprite, 0, 0)
+    tiles.placeOnTile(Firstenemy, tiles.getTileLocation(9, 5))
+    Firstenemy.follow(mySprite, 0)
+}
+function firewizard2 () {
+    firewizard = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f 2 2 2 2 2 2 f f f . . 
+        . . f f 2 2 2 2 2 2 2 2 2 f . . 
+        . . f 2 2 f f f f f f 2 2 f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . f f e e d d d d d d e e f f 2 
+        2 . . f e e 4 4 4 4 e e f . . 2 
+        2 2 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
+        2 2 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
+        . . . . f 2 2 5 5 2 2 f 2 2 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.empoweredPlayer)
+    spriteController(firewizard)
+    spritetype = "fire"
+}
+function earthCommonSpell1 () {
     animation.runImageAnimation(
     earthwizard,
     [img`
@@ -270,107 +417,50 @@ function earthCommonSpell2 () {
         . . . . . . . . . . . . . . . . 
         `, firewizard, -100, 0)
 }
-function spriteController (mySprite: Sprite) {
-    controller.moveSprite(mySprite)
-    scene.cameraFollowSprite(mySprite)
-    mySprite.setFlag(SpriteFlag.StayInScreen, true)
-}
-function fightScene (mySprite: Sprite) {
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(19, 5))
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.question2, function (sprite, otherSprite) {
-    game.showLongText("CHOOSE YOUR POWER", DialogLayout.Bottom)
-    story.showPlayerChoices("Fire", "Ice", "Earth")
-    if (story.checkLastAnswer("Fire")) {
-        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleRedCrystal)
-    } else if (story.checkLastAnswer("Ice")) {
-        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleInsignia)
-    } else if (story.checkLastAnswer("Earth")) {
-        tiles.placeOnRandomTile(_1st_sprrite, sprites.dungeon.collectibleBlueCrystal)
-    }
-})
-function earthwizard2 () {
-    earthwizard = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 7 7 f f f . . . . 
-        . . . f f f 7 8 8 7 f f f . . . 
-        . . f f f 7 8 7 7 8 7 f f f . . 
-        . . f f 7 7 8 7 7 8 7 7 7 f . . 
-        . . f 7 7 f f f f f f 7 7 f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . f f e e d d d d d d e e f f 7 
-        7 . . f e e 4 4 4 4 e e f . . 7 
-        7 7 7 7 f 7 8 8 8 8 7 f 7 7 7 7 
-        7 7 7 7 f 7 8 8 8 8 7 f 7 7 7 7 
-        . . . . f 7 7 5 5 7 7 f 7 7 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.empoweredPlayer)
-    spriteController(earthwizard)
-}
-sprites.onOverlap(SpriteKind.empoweredPlayer, SpriteKind.Enemy, function (sprite, otherSprite) {
-    color.startFade(color.originalPalette, color.Black, 500)
-    tiles.placeOnTile(sprite, tiles.getTileLocation(10, 5))
-    tiles.placeOnTile(otherSprite, tiles.getTileLocation(8, 5))
-    otherSprite.follow(sprite, 0)
-    fightscene12(sprite)
-    color.startFade(color.Black, color.originalPalette, 500)
-    pause(450)
-    Addition()
-})
-function Addition () {
-    addition1 = game.askForNumber("3 + 5", 2)
-    if (addition1 == 8) {
+function QuizAddition (num1: number, num2: number) {
+    addition1 = game.askForNumber("" + num1 + " + " + num2, 2)
+    if (addition1 == num1 + num2) {
         game.splash("Correct Answer!!")
-        correctcounter += 1
+        if (spritetype == "fire") {
+            fireCommanSpell1()
+        } else if (spritetype == "ice") {
+            iceCommonSpell1()
+        } else {
+            earthCommonSpell1()
+        }
         pause(200)
-        fireCommanSpell1()
+        if (correctcounter >= 0) {
+            pause(200)
+            for (let index = 0; index < 2; index++) {
+                damagecoin1 = sprites.createProjectileFromSprite(img`
+                    . . . . . . . 5 . . . . . . . . 
+                    . . . . . . 5 4 5 . . . . . . . 
+                    . . . . . 5 4 4 4 5 . . . . . . 
+                    . . . . 5 4 4 4 4 4 5 . . . . . 
+                    . . . 5 4 4 4 4 4 4 4 5 . . . . 
+                    . . 5 4 5 5 5 4 5 5 5 4 5 . . . 
+                    . 5 4 4 4 4 5 4 5 4 5 4 4 5 . . 
+                    5 4 4 4 5 5 5 4 5 4 5 4 4 4 5 . 
+                    . 5 4 4 5 4 4 4 5 4 5 4 4 5 . . 
+                    . . 5 4 5 5 5 4 5 5 5 4 5 . . . 
+                    . . . 5 4 4 4 4 4 4 4 5 . . . . 
+                    . . . . 5 4 4 4 4 4 5 . . . . . 
+                    . . . . . 5 4 4 4 5 . . . . . . 
+                    . . . . . . 5 4 5 . . . . . . . 
+                    . . . . . . . 5 . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `, Firstenemy, 0, -50)
+                pause(500)
+            }
+            pause(250)
+            correctcounter += 1
+            sprites.destroy(damagecoin1)
+        }
     } else {
         game.splash("Wrong Answer!")
     }
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
-    spriteCreator("abc")
-    afterpower(earthwizard)
-})
-function spriteCreator (text: string) {
-    if (text == "fire") {
-        firewizard2()
-    } else if (text == "ice") {
-        icewizard2()
-    } else {
-        earthwizard2()
-    }
-}
-function fightscene12 (mySprite: Sprite) {
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(13, 5))
-    controller.moveSprite(mySprite, 0, 0)
-    tiles.placeOnTile(Firstenemy, tiles.getTileLocation(9, 5))
-    Firstenemy.follow(mySprite, 0)
-}
-function firewizard2 () {
-    firewizard = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f 2 2 2 2 2 2 f f f . . 
-        . . f f 2 2 2 2 2 2 2 2 2 f . . 
-        . . f 2 2 f f f f f f 2 2 f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . f f e e d d d d d d e e f f 2 
-        2 . . f e e 4 4 4 4 e e f . . 2 
-        2 2 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
-        2 2 2 2 f 2 2 2 2 2 2 f 2 2 2 2 
-        . . . . f 2 2 5 5 2 2 f 2 2 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.empoweredPlayer)
-    spriteController(firewizard)
-}
+// Code written by Nate Au.
 function afterpower (mySprite: Sprite) {
     sprites.destroy(_1st_sprrite)
     sprites.destroy(questionbo_x)
@@ -654,6 +744,7 @@ function icewizard2 () {
         . . . . . f f . . f f . . . . 9 
         `, SpriteKind.empoweredPlayer)
     spriteController(icewizard)
+    spritetype = "ice"
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
     spriteCreator("fire")
@@ -873,15 +964,21 @@ function iceCommonSpell1 () {
 let projectileice: Sprite = null
 let icewizard: Sprite = null
 let projectilefire: Sprite = null
-let Firstenemy: Sprite = null
-let correctcounter = 0
+let damagecoin1: Sprite = null
 let addition1 = 0
-let firewizard: Sprite = null
 let projectileearth: Sprite = null
+let statusbarFirstEnemy: StatusBarSprite = null
+let firewizard: Sprite = null
+let spritetype = ""
 let earthwizard: Sprite = null
 let doungenentrence: Sprite = null
 let questionbo_x: Sprite = null
 let questionbox: Sprite = null
+let statusbarMysprite: StatusBarSprite = null
+let Firstenemy: Sprite = null
+let damagecoin2: Sprite = null
+let correctcounter = 0
+let substraction1 = 0
 let numbercounter = 0
 let tilemap2: tiles.TileMapData[] = []
 let _1st_sprrite: Sprite = null
